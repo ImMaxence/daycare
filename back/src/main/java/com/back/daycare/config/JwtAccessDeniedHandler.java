d39924cf9b0943a0fa838af48525891c044d7c13
mbonnici <maxence.bonnici2002@gmail.com>
2026-08-13 17:21:35 +0200

    init spring boot

--- back/src/main/java/com/back/daycare/config/JwtAccessDeniedHandler.java
+++ back/src/main/java/com/back/daycare/config/JwtAccessDeniedHandler.java
@@ -1,7 +1,7 @@
 package com.back.daycare.config;
 
 import com.back.daycare.dto.response.ErrorResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
